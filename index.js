c32f7af5473004326b5f39000c2c8b201f8e2173
Md Emon <mdshajdulhaqueemon@gmail.com>
2024-05-10 23:58:52 +0600

    update the data

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express()
 const cors=require("cors")
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-
+app.use(cors({
+  origin:[
+   'http://localhost:5173',
+   'https://course-wire.surge.sh',
+    'https://course-project-auth-726d2.firebaseapp.com',
+    'https://course-project-auth-726d2.web.app'
+  ],
+  credentials:true
+}));
 app.use(cors());
 app.use(express.json());
 
@@ -25,6 +33,7 @@ async function run() {
   try {
 
     const courseList=client.db("course-list").collection("course")
+    const courseEnrollment=client.db("Enrollment").collection("manageEnrollment")
 
 
     app.get('/course',async(req,res)=>{
@@ -32,6 +41,22 @@ async function run() {
         const result=await cursor.toArray()
         res.send(result)
        
+      })
+
+
+    app.get('/manageEnrollment',async(req,res)=>{
+        const cursor=courseEnrollment.find();
+        const result=await cursor.toArray()
+        res.send(result)
+       
+      })
+
+      app.post('/manageEnrollment',async(req,res)=>{
+        const enrollment=req.body;
+        console.log(enrollment)
+        const result=await courseEnrollment.insertOne(enrollment)
+
+        res.send(result)
       })
 
       app.get('/course/:id',async(req,res)=>{
